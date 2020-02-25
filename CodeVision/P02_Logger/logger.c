7d817f486ab18bfcb557b6fc31254665698b710a
Javier Mondragon <javiermomc@outlook.com>
2020-02-25 16:49:49 -0600

    ADC added

--- CodeVision/P02_Logger/logger.c
+++ CodeVision/P02_Logger/logger.c
@@ -7,8 +7,45 @@
 
 #include <90usb1286.h>
 
+// ADC
+
+// ADC variables
+
+
+// Voltage Reference: Int., cap. on AREF
+#define ADC_VREF_TYPE ((1<<REFS1) | (1<<REFS0) | (0<<ADLAR))
+
+
+// Read the AD conversion result
+unsigned int read_adc(unsigned char adc_input)
+{
+    ADMUX=adc_input | ADC_VREF_TYPE;
+    // Start the AD conversion
+    ADCSRA|=(1<<ADSC);
+    // Wait for the AD conversion to complete
+    while ((ADCSRA & (1<<ADIF))==0);
+    ADCSRA|=(1<<ADIF);
+    return ADCW;
+}
+
+
+
 void main(void)
 {
+
+// ADC
+
+// ADC initialization
+// ADC Clock frequency: 1000.000 kHz
+// ADC Voltage Reference: Int., cap. on AREF
+// ADC High Speed Mode: On
+// Digital input buffers on ADC0: On, ADC1: On, ADC2: On, ADC3: On
+// ADC4: On, ADC5: On, ADC6: On, ADC7: Off
+DIDR0=(1<<ADC7D) | (1<<ADC6D) | (0<<ADC5D) | (0<<ADC4D) | (0<<ADC3D) | (0<<ADC2D) | (0<<ADC1D) | (0<<ADC0D);
+ADMUX=ADC_VREF_TYPE;
+ADCSRA=(1<<ADEN) | (0<<ADSC) | (0<<ADATE) | (0<<ADIF) | (0<<ADIE) | (0<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+ADCSRB=(1<<ADHSM);
+
 while (1)
     {
     // Please write your application code here
---
+++ CodeVision/P02_Logger/logger.c~
@@ -0,0 +1,17 @@
+/*
+ * logger.c
+ *
+ * Created: 25-Feb-20 4:46:27 PM
+ * Author: javie
+ */
+
+#include <90usb1286.h>
+
+void main(void)
+{
+while (1)
+    {
+    // Please write your application code here
+
+    }
+}
