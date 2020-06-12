f4fc80ce5a4e26373be9a09cdd8cde53b4ac3a5a
erickr199 <61331382+erickr199@users.noreply.github.com>
2020-06-12 16:41:38 -0500

    ADC and USART added

--- CodeVision/P08_Atari/atari.c
+++ CodeVision/P08_Atari/atari.c
@@ -16,21 +16,60 @@
 #include "matrix.c"
 #include "game.c"
 
+
+
+//ADC
+// Voltage Reference: Int., cap. on AREF
+#define ADC_VREF_TYPE ((1<<REFS1) | (1<<REFS0) | (1<<ADLAR))
+
+// Read the 8 most significant bits
+// of the AD conversion result
+unsigned char read_adc(unsigned char adc_input)
+{
+ADMUX=adc_input | ADC_VREF_TYPE;
+// Delay needed for the stabilization of the ADC input voltage
+delay_us(10);
+// Start the AD conversion
+ADCSRA|=(1<<ADSC);
+// Wait for the AD conversion to complete
+while ((ADCSRA & (1<<ADIF))==0);
+ADCSRA|=(1<<ADIF);
+return ADCH;
+}
+
+
+
 void main(void)
 {
 
-PORTD=0x07;     //seteo de botones
+// USART1 initialization
+// Communication Parameters: 8 Data, 1 Stop, No Parity
+// USART1 Receiver: On
+// USART1 Transmitter: On
+// USART1 Mode: Asynchronous
+// USART1 Baud Rate: 9600
+UCSR1A=(0<<RXC1) | (0<<TXC1) | (0<<UDRE1) | (0<<FE1) | (0<<DOR1) | (0<<UPE1) | (0<<U2X1) | (0<<MPCM1);
+UCSR1B=(0<<RXCIE1) | (0<<TXCIE1) | (0<<UDRIE1) | (1<<RXEN1) | (1<<TXEN1) | (0<<UCSZ12) | (0<<RXB81) | (0<<TXB81);
+UCSR1C=(0<<UMSEL11) | (0<<UMSEL10) | (0<<UPM11) | (0<<UPM10) | (0<<USBS1) | (1<<UCSZ11) | (1<<UCSZ10) | (0<<UCPOL1);
+UBRR1H=0x00;
+UBRR1L=0x0C;
 
 // ADC initialization
 // ADC Clock frequency: 125.000 kHz
-// ADC Voltage Reference: AVCC pin
+// ADC Voltage Reference: Int., cap. on AREF
 // ADC High Speed Mode: Off
+// Only the 8 most significant bits of
+// the AD conversion result are used
 // Digital input buffers on ADC0: On, ADC1: On, ADC2: On, ADC3: On
-// ADC4: On, ADC5: On, ADC6: On, ADC7: Off
-DIDR0=(1<<ADC7D) | (0<<ADC6D) | (0<<ADC5D) | (0<<ADC4D) | (0<<ADC3D) | (0<<ADC2D) | (0<<ADC1D) | (0<<ADC0D);
+// ADC4: On, ADC5: On, ADC6: Off, ADC7: Off
+DIDR0=(1<<ADC7D) | (1<<ADC6D) | (0<<ADC5D) | (0<<ADC4D) | (0<<ADC3D) | (0<<ADC2D) | (0<<ADC1D) | (0<<ADC0D);
 ADMUX=ADC_VREF_TYPE;
 ADCSRA=(1<<ADEN) | (0<<ADSC) | (0<<ADATE) | (0<<ADIF) | (0<<ADIE) | (1<<ADPS2) | (0<<ADPS1) | (0<<ADPS0);
 ADCSRB=(1<<ADHSM);
+  
+  
+PORTD=0x07;     //seteo de botones
+
 
 ConfiguraMax();
 IniciaColumnas();
